d69781de1bb282f156464bffcc504ec876f7155d
DaneAlanS <Daneasimoneau@gmail.com>
2021-08-31 14:47:46 -0500

    Added fonts, bottom socials, link icons, adjusted grid

--- src/components/app.js
+++ src/components/app.js
@@ -10,6 +10,8 @@ import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from "./home.js"
 import Logo from '../../static/assets/YummyCryptoTransparent.png'
+import PCS from '../../static/assets/pancakeswap.png'
+import Bitmart from '../../static/assets/bitmart.png'
 
 function App() {
   
@@ -27,19 +29,19 @@ function App() {
                   <NavDropdown.Item href="#action/3.2"><a href="https://www.bitmart.com/trade/en?symbol=YUMMY_USDT" target="_blank" rel="noreferrer noopener"> Whitepaper</a> </NavDropdown.Item>
                 </NavDropdown>
                 <NavDropdown className ="drop-down" title="Buy Yummy" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1"><a href="https://pancakeswap.finance/swap" target="_blank" rel="noreferrer noopener"> PancakeSwap</a></NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2"><a href="https://www.bitmart.com/trade/en?symbol=YUMMY_USDT" target="_blank" rel="noreferrer noopener"> Bitmart</a> </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.1"><a href="https://pancakeswap.finance/swap" target="_blank" rel="noreferrer noopener"> <img className="pcs"src={PCS}></img>PancakeSwap</a></NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2"><a href="https://www.bitmart.com/trade/en?symbol=YUMMY_USDT" target="_blank" rel="noreferrer noopener"> <img className="bitmart"src={Bitmart}></img>Bitmart</a> </NavDropdown.Item>
                 </NavDropdown>
               </Nav>
             </Navbar.Collapse>
             <Navbar.Collapse className="justify-content-end">
               <Nav className="me-auto right-nav">
-                <Nav.Link href="#audit"><a href="https://twitter.com/YummyCrypto" target="_blank" rel="noreferrer noopener"><i className="fab fa-twitter"></i></a></Nav.Link>
-                <Nav.Link href="#audit"><a href="https://t.me/yummycoin" target="_blank" rel="noreferrer noopener"><i className="fab fa-telegram"></i></a></Nav.Link>
-                <Nav.Link href="#audit"><a href="https://www.reddit.com/r/yummycoin/" target="_blank" rel="noreferrer noopener"><i className="fab fa-reddit"></i></a></Nav.Link>
-                <Nav.Link href="#audit"><a href="https://www.facebook.com/OfficialYummyCoin" target="_blank" rel="noreferrer noopener"><i className="fab fa-facebook"></i></a></Nav.Link>
-                <Nav.Link href="#audit"><a href="https://www.instagram.com/yummycharity/" target="_blank" rel="noreferrer noopener"><i className="fab fa-instagram"></i></a></Nav.Link>
-                <Nav.Link href="#audit"><a href="https://www.youtube.com/c/YummyProjectOfficial/" target="_blank" rel="noreferrer noopener"><i className="fab fa-youtube"></i></a></Nav.Link>
+                <Nav.Link href="#action"><a href="https://twitter.com/YummyCrypto" target="_blank" rel="noreferrer noopener"><i className="fab fa-twitter"></i></a></Nav.Link>
+                <Nav.Link href="#action"><a href="https://t.me/yummycoin" target="_blank" rel="noreferrer noopener"><i className="fab fa-telegram"></i></a></Nav.Link>
+                <Nav.Link href="#action"><a href="https://www.reddit.com/r/yummycoin/" target="_blank" rel="noreferrer noopener"><i className="fab fa-reddit"></i></a></Nav.Link>
+                <Nav.Link href="#action"><a href="https://www.facebook.com/OfficialYummyCoin" target="_blank" rel="noreferrer noopener"><i className="fab fa-facebook"></i></a></Nav.Link>
+                <Nav.Link href="#action"><a href="https://www.instagram.com/yummycharity/" target="_blank" rel="noreferrer noopener"><i className="fab fa-instagram"></i></a></Nav.Link>
+                <Nav.Link href="#action"><a href="https://www.youtube.com/c/YummyProjectOfficial/" target="_blank" rel="noreferrer noopener"><i className="fab fa-youtube"></i></a></Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
--- src/components/home.js
+++ src/components/home.js
@@ -11,8 +11,10 @@ import {Navbar,Container, Popover, OverlayTrigger, Button} from 'react-bootstrap
 import Aos from "aos";
 import "aos/dist/aos.css";
 
-import LogoSquare from '../../static/assets/LogoSquare.png'
+import LogoSquare from '../../static/assets/LogoSquareTransparent.png'
 import BKFCLogo from '../../static/assets/BareKnuckle.png'
+import WhiteBG from '../../static/assets/BG-3.png'
+import BlueBG from '../../static/assets/BG-1.png'
 
 function home() {
 
@@ -25,22 +27,24 @@ return (
   <div className="home-wrapper">
     <div className="animate__animated animate__fadeIn  landing-wrapper">
     <div className="landing-text-container">
-            <div className="landing-phrase">Yummy is <strong>Changing Lives</strong> Across the Globe.</div>            
+            <div className="landing-phrase">Yummy is <span>Changing Lives</span> Across the Globe.</div>            
              <p>Yummy Crypto is a project launched on May 1st with a clear vision to deliver value to holders while providing contributions to help solve some of the world's most pressing issues.</p>
              <a className="join-our-cause">Learn how to join our cause  <i className="fas fa-angle-right"></i></a>
-          </div>
+            </div>
       <div className="landing-content-wrapper">
           <div className="video-wrapper">
             <iframe width="700" height="363" src="https://www.youtube.com/embed/oFu2nYNiIm8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={false}></iframe>
           </div>
       </div>
       </div>
-      <div className="divider-content-wrapper">
+      <div className="divider-content-wrapper"  style={{ backgroundImage: `url(${WhiteBG})` }}>
         <div data-aos="fade-right" data-aos-delay="100" data-aos-duration="1000" className="first-message">
           <img className="yummylogo"src={LogoSquare}></img>
-          <p>In our short history, we have already <strong>donated over $1.1M</strong> and <strong>35% of total token supply</strong> has been burned as a method to return value to our holders.</p>
+          <div className="divider-message">
+            <p>Within our short history, we've already <strong>donated over $1.1M</strong>, and <strong>35% of the total token supply has been burned</strong> as a method to return value to our holders. And that's just the start of it! </p>
+            <p>We recently introduced our <strong>Growth Fund</strong> to further maximize Yummy holders' returns through <strong>daily buybacks and burns.</strong> In addition, we have secured partnerships with prominent industry players such as Bare Knuckle FC further to advance our social exposure within the crypto space and have much more in the works!</p></div>          
         </div>
-        <div data-aos="zoom-in-up" data-aos-delay="700" data-aos-duration="1000" className="numbers-wrapper">
+        <div data-aos="zoom-in-up" data-aos-delay="400" data-aos-duration="1000" className="numbers-wrapper">
           <div className="numbers"><strong>$1,125,000</strong>
           <h3>AMOUNT DONATED</h3>
           <p>Total of our (blockchain verified) donations to Binance Lunch For Children.</p></div>
@@ -56,11 +60,21 @@ return (
             <span>CONTRACT ADDRESS: 0x05f2df7b3d612a23fe12162a6c996447dce728a5</span>
             <i className="far fa-clipboard"></i>
             </div>
-        <div className="darkBG">
-          <div className="darkBG-first-message">Yummy Crypto is just getting started. We recently introduced our Growth Fund to further maximize the return to Yummy holders through daily buybacks and burns. We have secured partnerships with prominent industry players such as Bare Knuckle FC to further advance our social exposure within the crypto space and have much more in the works!</div>
-          <div className="construction">UNDER CONSTRUCTION</div>
+        <div className="darkBG" style={{ backgroundImage: `url(${BlueBG})` }}>
+          <div className="darkBG-follow-socials">
+            FOLLOW OUR SOCIALS TO KEEP UP WITH US.
+            <div className="socials-big">
+              <a href="https://twitter.com/YummyCrypto" target="_blank" rel="noreferrer noopener"><i class="fab fa-twitter-square"></i></a>
+              <a href="https://t.me/yummycoin" target="_blank" rel="noreferrer noopener"><i class="fab fa-telegram-plane"></i></a>
+              <a href="https://www.reddit.com/r/yummycoin/" target="_blank" rel="noreferrer noopener"><i class="fab fa-reddit-square"></i></a>
+              <a href="https://www.facebook.com/OfficialYummyCoin" target="_blank" rel="noreferrer noopener"><i class="fab fa-facebook-square"></i></a>
+              <a href="https://www.instagram.com/yummycharity/" target="_blank" rel="noreferrer noopener"><i class="fab fa-instagram-square"></i></a>
+              <a href="https://www.youtube.com/c/YummyProjectOfficial/" target="_blank" rel="noreferrer noopener"><i class="fab fa-youtube-square"></i></a>
+            </div>
+          </div>
         <div className="full-socials">
           <a className="twitter-timeline" data-lang="en" data-width="340" data-height="500" data-theme="dark" href="https://twitter.com/YummyCrypto?ref_src=twsrc%5Etfw"></a>  
+             <a href="https://www.bareknuckle.tv/"  target="_blank" rel="noreferrer noopener"><img className="BKFC"src={BKFCLogo}></img></a>
           <iframe className="fb-page"src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FOfficialYummyCoin%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" scrolling="no" frameBorder="0" allowFullScreen={false} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
         </div>
          
